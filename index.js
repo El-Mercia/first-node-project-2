7b94093624bbbcd3c2ae449f508e58fd7d30d6e8
Mercia <merciarandrianome@gmail.com>
2020-03-19 17:07:08 +0100

    path

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 const express = require("express");
 
 const app = express();
+
+app.listen(3000, () => {
+	console.log("SERVER STARTED...");
+});
+
+app.get("/", (req, res) => {
+	console.log("access to / path");
+	res.send("hello world !");
+});
